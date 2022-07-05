7219064b4f9c3d06cfc3ef3a936296ea383b120c
deadapeipit <davidroring28@gmail.com>
2022-07-05 11:58:02 +0700

    CRUD with db

--- handler/userhandler.go
+++ handler/userhandler.go
@@ -1,38 +1,14 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 	"webserver/entity"
 
 	"github.com/gorilla/mux"
 )
-
-var users = map[int]entity.User{
-	1: {
-		Id:       1,
-		Username: "andi123",
-		Email:    "andi123@gmail.com",
-		Password: "password123",
-		Age:      9,
-	},
-	2: {
-		Id:       2,
-		Username: "budi123",
-		Email:    "budi123@gmail.com",
-		Password: "password123",
-		Age:      9,
-	},
-	3: {
-		Id:       3,
-		Username: "cantya123",
-		Email:    "cantya123@gmail.com",
-		Password: "password123",
-		Age:      9,
-	},
-}
 
 type UserHandlerInterface interface {
 	UsersHandler(w http.ResponseWriter, r *http.Request)
@@ -59,7 +35,11 @@ func (h *UserHandler) UsersHandler(w http.ResponseWriter, r *http.Request) {
 			h.getUsersHandler(w, r)
 		}
 	case http.MethodPost:
-		createUsersHandler(w, r)
+		if id != "" {
+			updateUserHandler(w, r, id)
+		} else {
+			createUsersHandler(w, r)
+		}
 	case http.MethodPut:
 		updateUserHandler(w, r, id)
 	case http.MethodDelete:
@@ -68,26 +48,29 @@ func (h *UserHandler) UsersHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *UserHandler) getUsersHandler(w http.ResponseWriter, r *http.Request) {
-	jsonData, _ := json.Marshal(&users)
-	w.Header().Add("Content-Type", "application/json")
-	w.Write(jsonData)
+	ctx := context.Background()
+	users, err := SqlConnect.GetUsers(ctx)
+	if err != nil {
+		writeJsonResp(w, statusError, err.Error())
+		return
+	}
+	writeJsonResp(w, statusSuccess, users)
 }
 
 func getUsersByIDHandler(w http.ResponseWriter, r *http.Request, id string) {
 	if idInt, err := strconv.Atoi(id); err == nil {
-		if user, ok := users[idInt]; ok {
-			jsonData, _ := json.Marshal(user)
-			w.Header().Add("Content-Type", "application/json")
-			w.Write(jsonData)
-			return
-		} else {
-			w.Write([]byte("No user found for given id"))
+		ctx := context.Background()
+		users, err := SqlConnect.GetUserByID(ctx, idInt)
+		if err != nil {
+			writeJsonResp(w, statusError, err.Error())
 			return
 		}
+		writeJsonResp(w, statusSuccess, users)
 	}
 }
 
 func createUsersHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
 	decoder := json.NewDecoder(r.Body)
 	var user entity.User
 	if err := decoder.Decode(&user); err != nil {
@@ -95,44 +78,54 @@ func createUsersHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if _, found := users[user.Id]; found {
-		w.Write([]byte("User with given id already exists"))
+	users, err := SqlConnect.CreateUser(ctx, user)
+	if err != nil {
+		writeJsonResp(w, statusError, err.Error())
 		return
 	}
-
-	users[user.Id] = user
-	var usersSlice []entity.User
-	for _, v := range users {
-		usersSlice = append(usersSlice, v)
-	}
-	jsonData, _ := json.Marshal(&usersSlice)
-	w.Header().Add("Content-Type", "application/json")
-	w.Write(jsonData)
+	writeJsonResp(w, statusSuccess, users)
 }
 
 func updateUserHandler(w http.ResponseWriter, r *http.Request, id string) {
+	ctx := context.Background()
+
 	if id != "" { // get by id
 		if idInt, err := strconv.Atoi(id); err == nil {
-			decoder := json.NewDecoder(r.Body)
-			var user entity.User
-			if err := decoder.Decode(&user); err != nil {
-				w.Write([]byte("error decoding json body"))
+			if users, err := SqlConnect.GetUserByID(ctx, idInt); err != nil {
+				writeJsonResp(w, statusError, err.Error())
 				return
-			}
+			} else if users != &(entity.User{}) {
+				writeJsonResp(w, statusError, "Data not exists")
+				return
+			} else {
+				decoder := json.NewDecoder(r.Body)
+				var user entity.User
+				if err := decoder.Decode(&user); err != nil {
+					w.Write([]byte("error decoding json body"))
+					return
+				}
 
-			users[idInt] = user
-			jsonData, _ := json.Marshal(&user)
-			w.Header().Add("Content-Type", "application/json")
-			w.Write(jsonData)
+				users, err := SqlConnect.UpdateUser(ctx, idInt, user)
+				if err != nil {
+					writeJsonResp(w, statusError, err.Error())
+					return
+				}
+				writeJsonResp(w, statusSuccess, users)
+			}
 		}
 	}
 }
 
 func deleteUserHandler(w http.ResponseWriter, r *http.Request, id string) {
+	ctx := context.Background()
 	if id != "" { // get by id
 		if idInt, err := strconv.Atoi(id); err == nil {
-			delete(users, idInt)
-			w.Write([]byte(fmt.Sprintf("User %d deleted", idInt)))
+			users, err := SqlConnect.DeleteUser(ctx, idInt)
+			if err != nil {
+				writeJsonResp(w, statusError, err.Error())
+				return
+			}
+			writeJsonResp(w, statusSuccess, users)
 		}
 	}
 }
